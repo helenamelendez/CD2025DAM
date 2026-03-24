b69f0ed5fc44f476f2e82560a8d2e5272ba225b4
HelenaMelendez <a25helenams@iessanclemente.net>
2026-03-24 13:01:12 +0100

    javadoc

--- UD5 - JUnit/e1/src/main/java/com/example/Factorial.java
+++ UD5 - JUnit/e1/src/main/java/com/example/Factorial.java
@@ -2,8 +2,18 @@ package com.example;
 
 import java.math.BigInteger;
 
+/**Clase Factorial
+ * @author A25HelenaMS
+ */
 public class Factorial {
 
+    /**
+     * Funcion factorial
+     * Calcula el factorial de un numero recibido por parametro
+     * @param n Numero a calcular
+     * @return  Numero BigInteger resultado del calculo
+     * @throws Exception Excepción que se lanza cuando el numero recibido por parametro es inferior a cero
+     */
     public BigInteger factorial(int n) throws Exception {
         if (n < 0) {
             throw new Exception("Error. El número tiene que ser >=0");
--- UD5 - JUnit/e1/src/main/java/com/example/Fibonacci.java
+++ UD5 - JUnit/e1/src/main/java/com/example/Fibonacci.java
@@ -2,8 +2,18 @@ package com.example;
 
 import java.math.BigInteger;
 
+/**Clase Fibonacci
+ * @author A25HelenaMS
+ */
 public class Fibonacci {
 
+    /**
+     * Funcion fibonacci
+     * Calcula la progrecion de fibonacci de un numero recibido por parametro
+     * @param n Numero a calcular
+     * @return Numero BigInteger resultado del calculo
+     * @throws Exception Excepción que se lanza cuando el numero recibido por parametro es inferior a cero
+     */
     public BigInteger fibonacci(int n) throws Exception {
         if (n < 0) {
             throw new Exception("Error. El número tiene que ser >=0");
