c681cf666dcf2ecb51b7acae66139e346bb9b30a
HelenaMelendez <a25helenams@iessanclemente.net>
2026-04-07 13:17:51 +0200

    Refator

--- UD5 - TDD/e1/src/main/java/com/example/ValidadorContraseña.java
+++ UD5 - TDD/e1/src/main/java/com/example/ValidadorContraseña.java
@@ -2,53 +2,28 @@ package com.example;
 
 public class ValidadorContraseña {
 
-public Boolean esValida(String valor){
-    Boolean noMenorDe8 =  valor.length() >= 8;
+   public Boolean esValida(String valor) {
 
-    Boolean contienMayscula = false;
-    for (int i = 0; i < valor.length(); i++) {
-       if (Character.isUpperCase(valor.charAt(i))){
-            contienMayscula = true;
-            break;
-       }
-    }
-    Boolean contienMinuscula = false;
-    for (int i = 0; i < valor.length(); i++) {
-       if (Character.isLowerCase(valor.charAt(i))){
-            contienMinuscula = true;
-            break;
-       }
-    }
+      Boolean noMenorDe8 = valor.length() >= 8;
+      Boolean contieneMayuscula = false;
+      Boolean contieneMinuscula = false;
+      Boolean contieneNumero = false;
+      Boolean contieneCaracterEspecial = valor.matches(".*[!@#$%^&*].*");
 
-    Boolean contienNumero = false;
-    for (int i = 0; i < valor.length(); i++) {
-       if (valor.matches(".*[0-9].*")){
-            contienNumero = true;
-            break;
-       }
+      for (int i = 0; i < valor.length(); i++) {
+         if (Character.isUpperCase(valor.charAt(i))) {
+            contieneMayuscula = true;
+         }
+         if (Character.isLowerCase(valor.charAt(i))) {
+            contieneMinuscula = true;
+         }
+         if (valor.matches(".*[0-9].*")) {
+            contieneNumero = true;
+         }
+      }
 
-    }
+      return noMenorDe8 && contieneMayuscula && contieneMinuscula && contieneNumero && contieneCaracterEspecial;
 
-    Boolean contienCaracterEspecial = false;
-    for (int i = 0; i < valor.length(); i++) {
-       if (valor.matches(".*[!@#$%^&*].*")){
-            contienCaracterEspecial = true;
-            break;
-       }
-
-    }
-
-    if (noMenorDe8 && contienMayscula && contienMinuscula && contienNumero && contienCaracterEspecial) {
-        return true;
-    }
-    else {
-        return false;
-    }
-
-}
-
-
-
-
+   }
 
 }
