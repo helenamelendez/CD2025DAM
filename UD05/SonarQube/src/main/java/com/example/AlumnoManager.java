da11221bcce3b34da9b347c95df974017ed41102
HelenaMelendez <a25helenams@iessanclemente.net>
2026-05-05 12:06:17 +0200

    SONAR

---
+++ UD05/SonarQube/src/main/java/com/example/AlumnoManager.java
@@ -0,0 +1,84 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class AlumnoManager {
+    private static final Logger LOGGER = Logger.getLogger(AlumnoManager.class.getName());
+    private List<Alumno> alumnos = new ArrayList<>();
+
+    public void agregarAlumno(Alumno a) {
+        if (a != null) {
+            alumnos.add(a);
+        }
+    }
+
+    public Alumno buscarAlumnoPorNombre(String nombre) {
+        for (Alumno a : alumnos) {
+            if (a.getNombre().equals(nombre)) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public double calcularMedia() {
+        double suma = 0;
+        for (Alumno a : alumnos) {
+            suma = suma + a.getNotaMedia();
+        }
+        if (alumnos.isEmpty()) {
+            return 0;
+        }
+        return suma / alumnos.size();
+    }
+
+    public String clasificarAlumno(Alumno a) {
+        if (a == null) {
+            return "Alumno nulo";
+        }
+
+        if (a.getNotaMedia() >= 9 && a.getEdad() >= 18) {
+            return "Sobresaliente adulto";
+        } else if (a.getNotaMedia() >= 9 && a.getEdad() < 18) {
+            return "Sobresaliente menor";
+        } else if (a.getNotaMedia() >= 7 && a.getEdad() >= 18) {
+            return "Notable adulto";
+        } else if (a.getNotaMedia() >= 7 && a.getEdad() < 18) {
+            return "Notable menor";
+        } else if (a.getNotaMedia() >= 5 && a.getEdad() >= 18) {
+            return "Aprobado adulto";
+        } else if (a.getNotaMedia() >= 5 && a.getEdad() < 18) {
+            return "Aprobado menor";
+        } else if (a.getEdad() >= 18) {
+            return "Suspenso adulto";
+        } else {
+            return "Suspenso menor";
+        }
+    }
+
+    public void mostrarAlumnosAprobados() {
+        for (Alumno a : alumnos) {
+            if (a.getNotaMedia() >= 5) {
+                LOGGER.info("Alumno aprobado: " + a.getNombre());
+            }
+        }
+    }
+
+    public void mostrarAlumnosSuspensos() {
+        for (Alumno a : alumnos) {
+            if (a.getNotaMedia() < 5) {
+                LOGGER.info("Alumno suspenso: " + a.getNombre());
+            }
+        }
+    }
+
+    public List<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(List<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
+}
