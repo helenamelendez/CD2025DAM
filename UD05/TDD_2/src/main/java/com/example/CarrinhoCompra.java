6cb44ffcedb34b38066d703612f0108a56f695c2
HelenaMelendez <a25helenams@iessanclemente.net>
2026-05-12 12:07:56 +0200

    TDD II - e7

--- UD05/TDD_2/src/main/java/com/example/CarrinhoCompra.java
+++ UD05/TDD_2/src/main/java/com/example/CarrinhoCompra.java
@@ -2,13 +2,27 @@ package com.example;
 
 import java.util.ArrayList;
 
+/**
+ * Clase que almancena la informacion de un Carrito de la compra
+ * @author A25HelenaMS
+ */
 public class CarrinhoCompra {
+
+    /**Lista de productos*/
     private ArrayList<Produto> listaProducto = new ArrayList<Produto>();
 
+    /**
+     * Funcion que recibe un Producto y lo añade a la lista
+     * @param p Producto a almacenar
+     */
     public void engadirProduto(Produto p){
         this.listaProducto.add(p);
     }
 
+    /**
+     * Funcion que calcula el precio total de los productos almacenados
+     * @return total del precio de los productos
+     */
     public Double calcularTotal(){
         Double total = 0.0;
         for (int i = 0; i < listaProducto.size(); i++) {
@@ -17,10 +31,18 @@ public class CarrinhoCompra {
         return total;
     }
     
+    /**
+     * Funcion que devuleve el numero total de los productos almacenados
+     * @return  total de productos en el carrito
+     */
     public int getNumeroProdutos(){
         return listaProducto.size();
     }
 
+    /**
+     * Funcion que elimina un producto de la lista de productos
+     * @param p Recive un producto a eliminar 
+     */
     public void eliminarProduto(Produto p){
         this.listaProducto.remove(p);
     }
--- UD05/TDD_2/src/main/java/com/example/Produto.java
+++ UD05/TDD_2/src/main/java/com/example/Produto.java
@@ -1,9 +1,19 @@
 package com.example;
 
+/**
+ * Clase que almancena la informacion de un producto
+ * @author A25HelenaMS
+ */
 public class Produto {
+    /** nombre del producto */
     private String nome;
+    /** precio del producto */
     private Double prezo;
 
+    /** Constructor con dos parametros
+     *  realiza comprobaciones sobre el nombre y el precio antes de asignarlos
+     *  en caso de no cumplir los criterios lanza una excepcion 
+     */
     public Produto(String nome, Double prezo){
         if (nome == null) {
             throw new IllegalArgumentException("o nome non pode ser null");
@@ -18,14 +28,27 @@ public class Produto {
         this.prezo = prezo;
     }
 
+    /**
+     * getter de nome
+     * @return  nombre del producto
+     */
     public String getNome(){
         return this.nome;
     }
 
+    /**
+     * getter de prezo
+     * @return precio del producto
+     */
     public Double getPrezo(){
         return this.prezo;
     }
 
+    /**
+     * setter de nome
+     * @param novoNome nuevo nombre a asignar
+     * realiza comprobaciones antes de la asignacion
+     */
     public void setNome(String novoNome){
         if (novoNome == "") {
             throw new IllegalArgumentException("o nome non pode ser valeiro");
@@ -34,6 +57,11 @@ public class Produto {
 
     }
 
+    /**
+     * setter de prezo
+     * @param novoPrezo nuevo precio a asignar
+     * realiza comprobaciones antes de la asignacion
+     */
     public void setPrezo(Double novoPrezo){
         if (novoPrezo < 0) {
             throw new IllegalArgumentException("o prezo non pode ser negativo");
